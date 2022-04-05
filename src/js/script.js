1ef9405a0f6e6450ccfa452b96c0708b2aaf4e82
nikeatcar <ssrnikita@gmail.com>
2022-04-06 01:04:57 +0400

    sliders and tabs

---
+++ src/js/script.js
@@ -0,0 +1,66 @@
+/* SLICK */
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        //dots: true, //точки внизу слайдера
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        //adaptiveHeight: true, //автовысота при переключении
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade:true,
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/4/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/4/next.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            }
+        ]
+    });
+
+/* TINY SLIDER */
+/* const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    nav: false,
+    speed: 1500,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    controls:false,
+    autoplayButtonOutput: false,
+    autoplayHoverPause: true,
+});
+
+document.querySelector('.prev').addEventListener ('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener ('click', function () {
+    slider.goTo('next');
+}); */
+
+/* КЛИКИ ПО ТАБАМ */
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+function toggleSlide(item){
+    $(item).each(function(i){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+});
